9ceba2614c97b081edc738cf99eb9f7f58887fdd
Tim <tim.ho@duke.edu>
2022-01-19 11:21:55 -0500

    added HDL checking function

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,5 +12,13 @@ def accept_input(test_name):
     entry = input("Enter the {} test result: ".format(test_name))
     return int(entry)
 
+def check_HDL(HDL_val):
+    if HDL_val >= 60:
+        return "Normal"
+    elif HDL_val >= 40 and HDL_val < 60:
+        return "Borderline Low"
+    else:
+        return "Low"
+
     
     
