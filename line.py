e8306fde9735ab8892612e2c5562130c06c8de0b
Tim <tim.ho@duke.edu>
2022-02-07 12:54:12 -0500

    fixed bugs for line prediction test by calling right function

---
+++ line.py
@@ -0,0 +1,7 @@
+def line(pt1, pt2, x):
+    if pt1[0] > pt2[0]:
+        slope = (pt1[1]-pt2[1])/(pt1[0]-pt2[0])
+        return pt2[1] + slope * (x - pt2[0]) 
+    else:
+        slope = (pt2[1]-pt1[1])/(pt2[0]-pt1[0])
+        return pt1[1] + slope * (x - pt1[0]) 
