ada62fd893517fc8a51d9845907a2ad737e1d610
Tim <tim.ho@duke.edu>
2022-02-07 12:51:43 -0500

    wrote point prediction function

--- test_line.py
+++ test_line.py
@@ -2,8 +2,8 @@ import pytest
 
 @pytest.mark.parametrize("tuple1, tuple2, x, expected", 
                         [[(0,0), (1,1), 0.5, 0.5],[(0,0), 
-                        (1,2), 0.5, 1],[(0,0), (1,4), 0.5, 2])
+                        (1,2), 0.5, 1],[(0,0), (1,4), 0.5, 2]])
 def test_line(tuple1, tuple2, x, expected):
-    from line import predict
-    answer = predict(tuple1, tuple2, x)
+    from line import line
+    answer = line(tuple1, tuple2, x)
     assert answer == expected
